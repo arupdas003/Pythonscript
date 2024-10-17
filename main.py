04285bd804ef0dbcc2dae734723482d58c1e03a3
arupdas003 <arup.das6489@gmail.com>
2024-10-17 12:05:04 +0530

    new progr

---
+++ main.py
@@ -0,0 +1,7 @@
+#The program is day1 of python learning and is designed to learn input and print parameters of python.
+print("Welcome to Band Name Generator")
+city_name = input("Whats the name of the city you grew up in ?\n")
+
+pet_name = input("What is your pets name ?\n")
+
+print("Your band name could be " + city_name + " " +pet_name)
